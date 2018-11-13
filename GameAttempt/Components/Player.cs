578ef952c434277e9533fc3052b115ac7e295cf5
Aaron O'Donnell <S00171335@mail.itsligo.ie>
2018-11-13 15:42:24 +0000

    Initial Commit

---
+++ GameAttempt/Components/Player.cs
@@ -0,0 +1,129 @@
+﻿using Components;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameAttempt.Components
+{
+    public sealed class Player : GameComponent
+    {
+        #region Properties and Variables
+
+        public PlayerIndex index;
+        public Vector2 Position;
+        public Texture2D Sprite { get; set; }
+        public string Name { get; set; }
+        public Body Body { get; set; }
+        Matrix Matrix { get; set; }
+        float gravity = 9.8f;
+        World world;
+
+        public List<Player> playerList = new List<Player>();
+
+        public bool IsConnected = false;
+
+        private int speed = 25;
+        #endregion
+
+        public Player(Game _game)
+            : base(_game)
+        {
+            GamePad.GetState(index);
+            _game.Components.Add(this);
+        }
+
+        public void GetPlayerPosition(Player player)
+        {
+            player.world = new World(new Vector2(0, gravity));
+
+            player.Position = new Vector2(200, 200);
+            player.Body = BodyFactory.CreateCircle(world, 1, 1);
+            player.Body.Restitution = 1f;
+            player.Body.BodyType = BodyType.Dynamic;
+
+            player.Position.X = Body.Position.X;
+            player.Position.Y = Body.Position.Y;
+        }
+
+        public void GetPlayerIndex(Player player)
+        {
+            #region Check GameStates
+            GamePadState state = GamePad.GetState(PlayerIndex.One);
+            GamePadState state2 = GamePad.GetState(PlayerIndex.Two);
+            GamePadState state3 = GamePad.GetState(PlayerIndex.Three);
+            GamePadState state4 = GamePad.GetState(PlayerIndex.Four);
+            
+
+            if (player.Name == "Player1" && state.IsConnected)
+            {
+                player.index = PlayerIndex.One;
+                player.IsConnected = true;
+            }
+            else if (player.Name == "Player2" && state2.IsConnected)
+            {
+                player.index = PlayerIndex.Two;
+                player.IsConnected = true;
+            }
+            else if (player.Name == "Players3" && !state.IsConnected)
+            {
+                player.index = PlayerIndex.Three;
+                player.IsConnected = true;
+            }
+            else if (player.Name == "Player4" && !state.IsConnected)
+            {
+                player.index = PlayerIndex.Four;
+                player.IsConnected = true;
+            }
+            #endregion
+        }
+
+        public unsafe void Update(GameTime gameTime, List<Player> playerList)
+        {
+            #region Player1 Controller
+            foreach (Player player in playerList)
+            {
+                if (player != null && player.IsConnected == true)
+                {
+                    player.world.Step(5f);
+
+                    player.Position.X = player.Body.Position.X;
+                    player.Position.Y = player.Body.Position.Y;
+
+                    GamePadState state = GamePad.GetState(player.index);
+                    player.Body.ApplyForce(state.ThumbSticks.Left * speed);
+
+                    if (InputManager.IsButtonPressed(Buttons.DPadRight))
+                    {
+                        player.Position.X += speed;
+                    }
+                    if (InputManager.IsButtonPressed(Buttons.DPadLeft))
+                    {
+                        player.Position.X -= speed;
+                    }
+                }
+            }
+
+            #endregion
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spritebatch, List<Player> playerList)
+        {
+            //spritebatch.Begin();
+            foreach (Player player in playerList)
+            {
+                if (player.Sprite != null && player.IsConnected == true)
+                {
+                    spritebatch.Draw(player.Sprite, player.Position, Color.White);
+                }
+            }
+            //spritebatch.End();
+        }
+    }
+}
---
+++ GameAttempt/Game1.cs
@@ -0,0 +1,123 @@
+﻿using Components;
+using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using GameAttempt.Components;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace GameAttempt
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Body floor;
+        Texture2D floorSprite;
+        Vector2 floorPos;
+
+        //World world;
+        //float gravity = 9.8f;
+
+        public Player player, player1, player2, player3, player4;
+
+        List<Player> playersList = new List<Player>();
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferHeight = 720;
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.ApplyChanges();
+
+            new InputManager(this);
+            player = new Player(this);
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            Vector2 size = GraphicsDevice.Viewport.Bounds.Size.ToVector2();
+            Vector2 pos = size - new Vector2(0, size.Y - 100);
+
+            floor = BodyFactory.CreateRectangle(new World(Vector2.Zero), size.X, size.Y, 0f, pos);
+
+            floorSprite = Content.Load<Texture2D>("Sprites/Floor");
+            floorPos.X = floor.Position.X;
+            floorPos.Y = floor.Position.Y;
+
+            #region Player Instances
+            player1 = new Player(this);
+            player1.Name = "Player1";
+            player1.Sprite = Content.Load<Texture2D>("Sprites/Mike");
+
+            player2 = new Player(this);
+            player2.Name = "Player2";
+            player2.Sprite = Content.Load<Texture2D>("Sprites/Spike");
+
+            player3 = new Player(this);
+            player3.Name = "Player3";
+
+            player4 = new Player(this);
+            player4.Name = "Player4";
+
+            #endregion
+
+            playersList.Add(player1);
+            playersList.Add(player2);
+            playersList.Add(player3);
+            playersList.Add(player4);
+
+            //world = new World(new Vector2(0, gravity));
+
+            foreach (Player player in playersList)
+            {
+                player.GetPlayerIndex(player);
+                player.GetPlayerPosition(player);
+            }
+
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        }
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            //player.Position.X = player.Body.Position.X;
+            //player.Position.Y = player.Body.Position.Y;
+
+            player.Update(gameTime, playersList);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            player.Draw(gameTime, spriteBatch, playersList);
+            spriteBatch.Draw(floorSprite, floorPos, Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
